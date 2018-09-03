c08c47bcbd1e0a45bf979e40b5f7a3e12fac048b
guo <guo@qq.com>
2018-09-03 21:14:38 +0800

    def sum in aaa.py

--- aaa.py
+++ aaa.py
@@ -2,3 +2,12 @@ def a_add(x,y):
     s = x+y
     print(s)
 a_add(3,2)
+
+def b_max(x,y,z):
+    max_num = x
+    if y>max_num:
+        max_num=y
+        if z>max_num:
+            max_num = z
+    print(max_num)
+b_max(6,30,29)
